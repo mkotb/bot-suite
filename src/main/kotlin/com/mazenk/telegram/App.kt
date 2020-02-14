3943e2e9313163f09f9e14f38672f935dd950191
Mazen Kotb <me@mazenk.com>
2020-02-14 01:26:34 -0800

    Add HTTP server

--- src/main/kotlin/com/mazenk/telegram/App.kt
+++ src/main/kotlin/com/mazenk/telegram/App.kt
@@ -3,6 +3,12 @@ package com.mazenk.telegram
 import com.jtelegram.api.TelegramBotRegistry
 import com.jtelegram.api.update.PollingUpdateProvider
 import com.mazenk.telegram.aesthethicc.AesthethiccBot
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
+import com.sun.net.httpserver.HttpServer
+import java.io.IOException
+import java.net.InetSocketAddress
+import kotlin.system.exitProcess
 
 fun main() {
     val registry = TelegramBotRegistry.builder()
@@ -10,5 +16,29 @@ fun main() {
             .build()
 
     AesthethiccBot(registry)
-    //
+    setupHealthServer()
+}
+
+fun setupHealthServer() {
+    val healthServer: HttpServer
+
+    try {
+         healthServer = HttpServer.create(InetSocketAddress(80), 0)
+    } catch (e: IOException) {
+        println("Unable to start health server! Shutting down...")
+        e.printStackTrace()
+        exitProcess(-127)
+    }
+
+    healthServer.createContext("/").handler = HttpHandler {
+        val response = "OK".toByteArray()
+        val output = it.responseBody
+
+        it.sendResponseHeaders(200, response.size.toLong())
+        output.write(response)
+        output.flush()
+        it.close()
+    }
+
+    healthServer.start()
 }
